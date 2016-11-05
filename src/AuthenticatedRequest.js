3f496802632f46e915f6118705a8e2adfc8025d8
Christian Smith <smith@anvil.io>
2016-11-04 21:48:31 -0700

    wip(AuthenticatedRequest): handle JWT decoding error, add comments, fix typos

--- src/AuthenticatedRequest.js
+++ src/AuthenticatedRequest.js
@@ -8,6 +8,9 @@ const {JWT} = require('jose')
  */
 class AuthenticatedRequest {
 
+  /**
+   * constructor
+   */
   constructor (rs, req, res, next, options) {
     this.rs = rs
     this.req = req
@@ -16,10 +19,27 @@ class AuthenticatedRequest {
     this.options = options
   }
 
-
+  /**
+   * authenticate
+   *
+   * @description
+   * Authenticate an HTTP request by validating a signed JWT bearer
+   * token. Handles error responses and, when authentication succeeds,
+   * passes control to the middleware stack.
+   *
+   * @param {ResourceServer} rs
+   * @param {IncomingMessage} req
+   * @param {ServerResponse} res
+   * @param {Function} next
+   * @param {Object} options
+   */
   static authenticate (rs, req, res, next, options) {
     let request = new AuthenticatedRequest(rs, req, res, next, options)
 
+    // These methods on the request object are invoked in the promise chain
+    // as callbacks. Each method in the chain takes a request instance and
+    // assuming no error conditions are encountered, returns it, or returns
+    // a promise that resolves it.
     Promise.resolve(request)
       .then(request.validateAuthorizationHeader)
       .then(request.validateQueryParameter)
@@ -39,6 +59,13 @@ class AuthenticatedRequest {
 
   /**
    * validateAuthorizationHeader
+   *
+   * @description
+   * Validate HTTP Authorization Header and extract bearer token credentials.
+   * Trigger an error response in the event the header is misused.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateAuthorizationHeader (request) {
     let {token, req} = request
@@ -68,6 +95,15 @@ class AuthenticatedRequest {
 
   /**
    * validateQueryParameter
+   *
+   * @description
+   * Validate HTTP Query Parameter and extract bearer token credentials.
+   * Trigger an error response in the event the parameter is misused. This
+   * authentication is disallowed by default and must be explicitly enabled
+   * by setting the `query` option to `true`.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateQueryParameter (request) {
     let {token, req, options} = request
@@ -76,7 +112,7 @@ class AuthenticatedRequest {
     // 💀 💀 💀 💀 💀 💀 💀         WARNING          💀 💀 💀 💀 💀 💀 💀 💀
     //
     // DO NOT ALLOW THIS AUTHENTICATION METHOD UNLESS THE USER
-    // EXPLICITLY ENABLES IT. CHANCES ARE IT'S USE IS NOT SECURE.
+    // EXPLICITLY ENABLES IT. CHANCES ARE ITS USE IS NOT SECURE.
     //
     // SEE RFC 6750 SECTIONS 2.3 AND 5.3 FOR DETAILS.
     //
@@ -102,6 +138,13 @@ class AuthenticatedRequest {
 
   /**
    * validateBodyParameter
+   *
+   * @description
+   * Validate HTTP Form Post Parameter and extract bearer token credentials.
+   * Trigger an error response in the event the form parameter is misused.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateBodyParameter (request) {
     let {token, req} = request
@@ -125,6 +168,13 @@ class AuthenticatedRequest {
 
   /**
    * requireAccessToken
+   *
+   * @description
+   * Ensure a bearer token is included in the request unless authentication
+   * is optional.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   requireAccessToken (request) {
     let {token, options} = request
@@ -139,6 +189,12 @@ class AuthenticatedRequest {
 
   /**
    * validateAccessToken
+   *
+   * @description
+   * Validate all aspects of an access token.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateAccessToken (request) {
     let {token, providers, options} = request
@@ -153,21 +209,36 @@ class AuthenticatedRequest {
       .then(request.validateExpiry)
       .then(request.validateNotBefore)
       .then(request.validateScope)
-      .catch(internalServerError)
   }
 
   /**
    * decode
+   *
+   * @description
+   * Decode a JWT Bearer Token and set the decoded object on the
+   * AuthenticatedRequest instance.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   decode (request) {
-    let {token} = request
+    let {token, jwt, options: {realm}} = request
 
     // decode and validate the token
-    let jwt = JWT.decode(token)
+    try {
+      jwt = JWT.decode(token)
+    } catch (error) {
+      return request.unauthorized({
+        realm,
+        error: 'invalid_token',
+        error_description: 'Access token is not a JWT'
+      })
+    }
+
     let validation = jwt.validate()
 
     if (!validation.valid) {
-      request.badRequest('Can\'t decode bearer token as a JWT')
+      return request.badRequest('Access token is invalid')
     }
 
     request.jwt = jwt
@@ -176,6 +247,13 @@ class AuthenticatedRequest {
 
   /**
    * allow
+   *
+   * @description
+   * Enforce access restrictions for issuers, audience, and subjects
+   * configured using the "allow" option.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   allow (request) {
     let {jwt, options} = request
@@ -207,6 +285,13 @@ class AuthenticatedRequest {
 
   /**
    * deny
+   *
+   * @description
+   * Enforce access restrictions for issuers, audience, and subjects
+   * configured using the "deny" option.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   deny (request) {
     let {jwt, options} = request
@@ -238,9 +323,29 @@ class AuthenticatedRequest {
 
   /**
    * resolveKeys
+   *
+   * @description
+   * Obtains the cryptographic key necessary to validate the JWT access token's
+   * signature.
+   *
+   * Based on the "iss" claim in the JWT access token payload, obtain OpenID
+   * Connect configuration and the JWT Set for the corresponding provider.
+   * This data is cached by the ResourceServer. The cache can be persisted and
+   * restored.
+   *
+   * In the event no suitable key can be matched based on the JWT "kid" header
+   * or JWK "use" property, refresh the cached configuration and JWK Set for
+   * the issuer and try again. If a key still cannot be found, authentication
+   * fails.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   resolveKeys (request) {
-    let {rs: {providers}, options: {realm}} = request
+    let providers = request.rs.providers
+    let realm = request.options.realm
+    let jwt = request.jwt
+    let iss = jwt.payload.iss
 
     return providers.resolve(iss).then(provider => {
       // key matched
@@ -269,6 +374,12 @@ class AuthenticatedRequest {
 
   /**
    * verifySignature
+   *
+   * @description
+   * Verify the access token signature.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   verifySignature (request) {
     let {jwt, options: {realm}} = request
@@ -284,6 +395,12 @@ class AuthenticatedRequest {
 
   /**
    * validateExpiry
+   *
+   * @description
+   * Ensures the access token has not expired.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateExpiry (request) {
     let {jwt, options: {realm}} = request
@@ -302,6 +419,12 @@ class AuthenticatedRequest {
 
   /**
    * validateNotBefore
+   *
+   * @description
+   * Ensures the access token has become active.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateNotBefore (request) {
     let {jwt, options: {realm}} = request
@@ -321,6 +444,12 @@ class AuthenticatedRequest {
 
   /**
    * validateScope
+   *
+   * @description
+   * Ensures the access token has sufficient scope.
+   *
+   * @param {AuthenticatedRequest} request
+   * @returns {AuthenticatedRequest}
    */
   validateScope (request) {
     let {jwt, options: {realm, scopes}} = request
@@ -346,9 +475,13 @@ class AuthenticatedRequest {
     return request
   }
 
-
   /**
    * success
+   *
+   * @description
+   * Pass control to the next middleware.
+   *
+   * @param {AuthenticatedRequest} request
    */
   success (request) {
     request.next()
@@ -356,6 +489,12 @@ class AuthenticatedRequest {
 
   /**
    * 400 Bad Request
+   *
+   * @description
+   * Respond with 400 status code.
+   *
+   * @param {string} description
+   * @returns {Promise}
    */
   badRequest (description) {
     let {res} = this
@@ -370,12 +509,18 @@ class AuthenticatedRequest {
 
   /**
    * 401 Unauthorized
+   *
+   * @description
+   * Respond with 401 status code and WWW-Authenticate challenge.
+   *
+   * @param {Object} params
+   * @returns {Promise}
    */
   unauthorized (params = {}) {
     let {res} = this
 
     res.set({
-      'WWW-Authenticate', `Bearer ${this.encodeChallengeParams(params)}`
+      'WWW-Authenticate': `Bearer ${this.encodeChallengeParams(params)}`
     })
 
     res.status(401).send('Unauthorized')
@@ -384,12 +529,18 @@ class AuthenticatedRequest {
 
   /**
    * 403 Forbidden
+   *
+   * @description
+   * Respond with 401 status code and WWW-Authenticate challenge.
+   *
+   * @param {Object} params
+   * @returns {Promise}
    */
   forbidden (params = {}) {
     let {res} = this
 
     res.set({
-      'WWW-Authenticate', `Bearer ${this.encodeChallengeParams(params)}`
+      'WWW-Authenticate': `Bearer ${this.encodeChallengeParams(params)}`
     })
 
     res.status(403).send('Forbidden')
@@ -398,13 +549,25 @@ class AuthenticatedRequest {
 
   /**
    * 500 Internal Server Error
+   *
+   * @description
+   * Respond with 500 status code.
+   *
+   * @param {Error} error
    */
   internalServerError (error) {
+    console.log(error)
     this.res.status(500).send('Internal Server Error')
   }
 
   /**
    * encodeChallengeParams
+   *
+   * @description
+   * Encode parameters for WWW-Authenticate challenge header.
+   *
+   * @param {Object} params
+   * @return {string}
    */
   encodeChallengeParams (params) {
     let pairs = []
